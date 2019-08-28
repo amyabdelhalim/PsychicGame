46535d118217bb23e55c5cbdf10a59f4ba95f378
ames <amyabdelhalim@Amys-MacBook-Air.local>
2019-08-28 15:16:09 -0700

    console log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,26 @@
-console.log("Hi")
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
+//  square bracket = Array
+// curly bracket= object 
+var random = Math.random() * alphabet.length
+// console.log(random)
+var floorRandom = Math.floor(random)
+// console.log(floorRandom)
+
+// var random = Math.floor( Math.random() * alphabet.length )
+
+var computerChoice = alphabet[floorRandom]
+console.log(computerChoice)
+
+document.onkeyup = function (event) {
+    console.log(event.key)
+    var userChoice = event.key
+
+    if (computerChoice === userChoice) {
+        console.log("You WIN!")
+    }
+    else {
+        console.log("You LOSE!")
+    }
+}
+// if condition = true else=false
+//  === is a comparison opperator  and the result is a Boolean(T/F)
